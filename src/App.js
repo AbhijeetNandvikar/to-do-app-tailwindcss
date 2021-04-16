74a6c20d36cb6b17dc380d04567ade14ad404a1b
AbhijeetNandvikar <abhijeetvn1234@gmail.com>
2021-04-16 21:14:10 +0530

    to-do-app initialized

--- src/App.js
+++ src/App.js
@@ -1,23 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import React from "react";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+  const [list, setList] = React.useState([]);
+
+  const [input, setInput] = React.useState("");
+  const showList = (list) => {
+    if (!list.length) return <></>;
+    return list.map((item, i) => {
+      return (
+        <div
+          key={i}
+          className="w-96 h-24 border-2 border-white rounded-md flex items-center justify-between px-4 mb-4"
         >
-          Learn React
-        </a>
-      </header>
+          <div>
+            <div className="text-lg text-left">
+              {i + 1 + ".  "}
+              {item.name}
+            </div>
+            <div className="text-sm mt-3 self-start text-left">{item.time}</div>
+          </div>
+          <button
+            onClick={() => {
+              let currentList = list;
+              currentList = currentList.filter((item, index) => index !== i);
+              setList(currentList);
+            }}
+          >
+            Delete
+          </button>
+        </div>
+      );
+    });
+  };
+
+  const AddListItem = (input) => {
+    let currentList = list;
+    if (!input) return null;
+    currentList.push({
+      name: input,
+      time: new Date().toLocaleString("en-US"),
+    });
+    setList(currentList);
+    setInput("");
+  };
+  return (
+    <div className="App h-screen flex items-center flex-col justify-center bg-blue-500 text-white">
+      <h1 className="font-bold text-xl mb-6">To Do App</h1>
+      <div>
+        <input
+          onChange={(e) => setInput(e.target.value)}
+          className="border-2 h-8 rounded-md mr-6 px-4 outline-none text-gray-500"
+          value={input}
+        />
+        <button
+          className="w-20 h-8 border-2 rounded-md"
+          onClick={() => AddListItem(input)}
+        >
+          Add
+        </button>
+      </div>
+      <div className="h-3/4 w-auto mt-8  px-8 scrollbar-thin scrollbar-thumb-white scrollbar-track-transparent overflow-y-scroll scrollbar-thumb-rounded">
+        {showList(list)}
+      </div>
     </div>
   );
 }
